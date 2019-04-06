199cbda39a0e28e4e668542dd647f431141f4bba
Kyujin Cho <thy2134@gmail.com>
2019-04-07 02:51:28 +0900

    Detect node installation and install pug-cli

--- install.sh
+++ install.sh
@@ -8,7 +8,16 @@ if ! [ -x "$(command -v git)" ]; then
     echo "Error: git is not installed." >&2
     exit 1
 fi 
+if ! [ -x "$(command -v node)" ]; then
+    echo "Error: node is not installed." >&2
+    exit 1
+fi
 
-echo "Starting MySQL server on docker container..."
+if ! [ -x "$(command -v pug)" ]; then 
+    echo "Installing pug-cli via NPM..."
+    npm i -g pug-cli
+fi
+
+echo "Starting MongoDB server on using docker..."
 docker run --name dockerserve-mongo -p 127.0.0.1:42932:27017 -d mongo:latest
 echo "Installation complete!"
